51fc6433776e34d7d557a87a636348ee9e19c8e3
Juan Salazar <ferthelet@gmail.com>
2023-03-06 07:35:29 -0600

    adds addition, substraction, multiplication, and division

---
+++ index.js
@@ -0,0 +1,17 @@
+// js
+
+function add(addend1, addend2) {
+    return addend1 + addend2; // sum
+}
+
+function subtract(minuend, sustraned) {
+    return minuend - sustraned; // difference
+}
+
+function multiply(factor1, factor2) {
+    return factor1 * factor2; // product
+}
+
+function divide(dividend, divisor) {
+    return dividend / divisor; // quotient
+}
